6a5d93920273855026a61e7f25b4a0ce97862c08
yasminandrade8 <yasminrandrade014@gmail.com>
2026-09-12 16:13:17 -0300

    Update

--- ex04/app.js
+++ ex04/app.js
@@ -8,56 +8,24 @@ lógica de tratamento da URL da requisição (req.url) e o envio dos códigos de
 
 const http = require('http');
 const url = require('url');
-const path = require('url');
-const fs = require('fs');
-
-const publicDir = path.join(__dirname, 'public');
-const contentTypes = {
-    '.html': 'text/html; charset=utf-8',
-    '.json': 'application/json; charset=utf-8',
-    '.css': 'text/css; charset=utf-8',
-    '.js': 'application/javascript; charset=utf-8'
-};
 
 const routes = {
-    '/': 'index.html',
-    '/fatec': 'fatec.html',
-    '/fecap': 'fecap.html'
-}
+    '/fatec': 'Bem-vindo à Faculdade de Tecnologia',
+    '/fecap': 'Bem-vindo a FATEC Diadema'
+};
 
-function readFile(response, file) {
-    fs.readFile(file, function(err, data){
-        if (err) {
-            response.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
-            return fs.createReadStream(
-                path.join(publicDir, 'erro404.html')
-            ).pipe(response);
-        }
-
-        var extension = path.extname(file).toLowerCase();
-        var contentType = contentTypes[extension] || 'application/octet-stream';
-
-        response.writeHead(200, {'Content-Type': contentType});
-        response.end(data);
-    })
-}
-
-var callback = function(request, response) {
-    var pathname = decodeURIComponent(url.parse(request.url).pathname);
+const server = http.createServer((request, response) => {
+    const pathname = url.parse(request.url).pathname;
 
     if (routes[pathname]) {
-        return readFile(response, path.join(publicDir, routes[pathname]));
+        response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+        response.end(routes[pathname]);
+    } else {
+        response.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
+        response.end('Recurso não encontrado no servidor');
     }
+});
 
-    var file = path.join(publicDir, pathname);
-
-    if (!file.startsWith(publicDir)) {
-        return readFile((response, path.join(publicDir, 'erro404.html')));
-    }
-
-    readFile(response, file);
-}
-
-var server = http.createServer(callback);
-server.listen(3000);
-console.log(`Servidor iniciado em: http://localhost:3000/`)
+server.listen(3000, () => {
+    console.log('Servidor iniciado em: http://localhost:3000/');
+});
